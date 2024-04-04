ecc995fddb42ccab4df7d88e5188184a4961afa6
blee203 <vothibichly43@gmail.com>
2024-04-04 22:55:49 +0700

    init

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:demo_firebase/pages/login_page.dart';
+import 'package:demo_firebase/screen/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart'; // Import Firebase Auth
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Firebase',
+      routes: {
+        '/': (context) => SplashScreen(
+          // Here, you can decide whether to show the LoginPage or HomePage based on user authentication
+          child: LoginPage(),
+        ),
+        '/login': (context) => LoginPage(),
+        // '/signUp': (context) => SignUpPage(),
+        // '/home': (context) => HomePage(),
+      },
+    );
+  }
+}
